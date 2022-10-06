983097c1723bad7e4e95cced4e6a13c0f4f50c54
aeon <jerry73204@gmail.com>
2022-10-06 11:13:02 +0800

    Add color mode in kiss3d gui

--- viz/src/kiss3d_gui.rs
+++ viz/src/kiss3d_gui.rs
@@ -3,14 +3,17 @@ use async_std::task::{spawn_blocking, JoinHandle};
 use itertools::Itertools as _;
 use kiss3d::{
     camera::{ArcBall, Camera},
+    event::{Action, Key, Modifiers, WindowEvent},
     light::Light,
     nalgebra as na,
     planar_camera::PlanarCamera,
     post_processing::PostProcessingEffect,
     window::Window,
 };
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 use r2r::{
-    autoware_auto_perception_msgs::msg::DetectedObjects,
+    autoware_auto_perception_msgs::msg::{DetectedObject, DetectedObjects, ObjectClassification},
     geometry_msgs::msg::{Point, Quaternion, Vector3},
     log_warn,
     sensor_msgs::msg::{PointCloud2, PointField},
@@ -32,9 +35,10 @@ pub fn start() -> (JoinHandle<Result<()>>, flume::Sender<Message>) {
         camera.set_up_axis(na::Vector3::new(0.0, 0.0, 1.0));
         let state = State {
             points: vec![],
-            segment_sets: vec![],
+            objects: vec![],
             rx,
             camera,
+            point_color_mode: PointColorMode::default(),
         };
         window.render_loop(state);
         anyhow::Ok(())
@@ -44,13 +48,39 @@ pub fn start() -> (JoinHandle<Result<()>>, flume::Sender<Message>) {
 }
 
 struct State {
+    point_color_mode: PointColorMode,
     points: Vec<ColoredPoint>,
-    segment_sets: Vec<ColoredSegmentSet>,
+    objects: Vec<Object3D>,
     rx: flume::Receiver<Message>,
     camera: ArcBall,
 }
 
 impl State {
+    fn process_events(&mut self, window: &mut Window) {
+        window.events().iter().for_each(|evt| {
+            use Action as A;
+            use Key as K;
+            use Modifiers as M;
+            use WindowEvent as E;
+
+            match evt.value {
+                E::Key(key, action, mods) => {
+                    let control = !(mods & M::Control).is_empty();
+                    let shift = !(mods & M::Shift).is_empty();
+                    let super_ = !(mods & M::Super).is_empty();
+
+                    match (key, action, control, shift, super_) {
+                        (K::C, A::Press, false, false, false) => {}
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        });
+    }
+
+    fn process_key_event() {}
+
     fn update_msg(&mut self, msg: Message) {
         match msg {
             Message::PointCloud2(pcd) => self.update_point_cloud(pcd),
@@ -132,87 +162,29 @@ impl State {
     }
 
     fn update_aw_objs(&mut self, objs: DetectedObjects) {
-        self.segment_sets = objs
-            .objects
-            .iter()
-            .map(|obj| {
-                // construct transform
-                let transform = {
-                    // get position
-                    let translation = {
-                        let Point { x, y, z } = obj.kinematics.pose_with_covariance.pose.position;
-                        na::Translation3::new(x as f32, y as f32, z as f32)
-                    };
-
-                    // get position
-                    let rotation = {
-                        let Quaternion { x, y, z, w } =
-                            obj.kinematics.pose_with_covariance.pose.orientation;
-                        na::UnitQuaternion::from_quaternion(na::Quaternion::new(
-                            w as f32, x as f32, y as f32, z as f32,
-                        ))
-                    };
-
-                    na::Isometry3 {
-                        rotation,
-                        translation,
-                    }
-                };
-
-                // get size
-                let Vector3 {
-                    x: sx,
-                    y: sy,
-                    z: sz,
-                } = obj.shape.dimensions;
-
-                // compute bbox segments
-                let segments: Vec<[na::Point3<f32>; 2]> =
-                    [[false, false], [false, true], [true, true], [true, false]]
-                        .iter()
-                        .circular_tuple_windows()
-                        .take(4)
-                        .flat_map(|(pbits, nbits)| {
-                            let [px, py] = *pbits;
-                            let [nx, ny] = *nbits;
-
-                            [
-                                [[px, py, false], [nx, ny, false]],
-                                [[px, py, true], [nx, ny, true]],
-                                [[px, py, false], [px, py, true]],
-                            ]
-                        })
-                        .map(|[pbits, nbits]| {
-                            let bit2one = |bit: bool| {
-                                if bit {
-                                    1f32
-                                } else {
-                                    -1f32
-                                }
-                            };
-                            let to_point = |[xbit, ybit, zbit]: [bool; 3]| {
-                                let point = na::Point3::new(
-                                    bit2one(xbit) * sx as f32,
-                                    bit2one(ybit) * sy as f32,
-                                    bit2one(zbit) * sz as f32,
-                                );
-                                transform * point
-                            };
-                            let ppoint = to_point(pbits);
-                            let npoint = to_point(nbits);
-                            [ppoint, npoint]
-                        })
-                        .collect();
-
-                let color = na::Point3::new(1.0, 1.0, 0.0);
-
-                ColoredSegmentSet { segments, color }
-            })
-            .collect();
+        self.objects = objs.objects.iter().map(Object3D::from).collect();
     }
 
     fn render(&self, window: &mut Window) {
         // Draw axis
+        self.draw_axis(window);
+
+        // Draw points
+        self.points.iter().for_each(|point| {
+            let ColoredPoint { position, color } = point;
+            window.draw_point(position, color);
+        });
+
+        // Draw 3D bbox segments
+        self.objects.iter().for_each(|obj| {
+            let ColoredSegmentSet { segments, color } = &obj.bbox_segments;
+            segments.iter().for_each(|[pa, pb]| {
+                window.draw_line(pa, pb, color);
+            });
+        });
+    }
+
+    fn draw_axis(&self, window: &mut Window) {
         let origin = na::Point3::new(0.0, 0.0, 0.0);
         window.draw_line(
             &origin,
@@ -229,19 +201,6 @@ impl State {
             &na::Point3::new(0.0, 0.0, 1.0),
             &na::Point3::new(0.0, 0.0, 1.0),
         );
-
-        // Draw points
-        self.points.iter().for_each(|point| {
-            let ColoredPoint { position, color } = point;
-            window.draw_point(position, color);
-        });
-
-        // Draw segments
-        self.segment_sets.iter().for_each(|set| {
-            set.segments.iter().for_each(|[pa, pb]| {
-                window.draw_line(pa, pb, &set.color);
-            });
-        });
     }
 }
 
@@ -286,6 +245,7 @@ struct ColoredPoint {
     pub color: na::Point3<f32>,
 }
 
+#[derive(Clone)]
 struct ColoredSegmentSet {
     pub segments: Vec<[na::Point3<f32>; 2]>,
     pub color: na::Point3<f32>,
@@ -305,5 +265,143 @@ impl From<DetectedObjects> for Message {
 impl From<PointCloud2> for Message {
     fn from(v: PointCloud2) -> Self {
         Self::PointCloud2(v)
+    }
+}
+
+struct Object3D {
+    bbox_segments: ColoredSegmentSet,
+    transform: na::Isometry3<f32>,
+    size_xyz: [f32; 3],
+    classification: Vec<ObjectClassification>,
+}
+
+impl Object3D {
+    pub fn contains_point(&self, point: &na::Point3<f32>) -> bool {
+        let point = self.transform.inverse() * point;
+        let [sx, sy, sz] = self.size_xyz;
+
+        let check_range = |sz: f32, value: f32| {
+            let hsz = sz / 2.0;
+            ((-hsz)..=hsz).contains(&value)
+        };
+
+        check_range(sx, point.x) && check_range(sy, point.y) && check_range(sz, point.z)
+    }
+}
+
+impl From<DetectedObject> for Object3D {
+    fn from(iobj: DetectedObject) -> Self {
+        Self::from(&iobj)
+    }
+}
+
+impl From<&DetectedObject> for Object3D {
+    fn from(iobj: &DetectedObject) -> Self {
+        // construct transform
+        let transform = {
+            // get position
+            let translation = {
+                let Point { x, y, z } = iobj.kinematics.pose_with_covariance.pose.position;
+                na::Translation3::new(x as f32, y as f32, z as f32)
+            };
+
+            // get position
+            let rotation = {
+                let Quaternion { x, y, z, w } =
+                    iobj.kinematics.pose_with_covariance.pose.orientation;
+                na::UnitQuaternion::from_quaternion(na::Quaternion::new(
+                    w as f32, x as f32, y as f32, z as f32,
+                ))
+            };
+
+            na::Isometry3 {
+                rotation,
+                translation,
+            }
+        };
+
+        // get size
+        let Vector3 {
+            x: sx,
+            y: sy,
+            z: sz,
+        } = iobj.shape.dimensions;
+        let sx = sx as f32;
+        let sy = sy as f32;
+        let sz = sz as f32;
+
+        // compute bbox segments
+        let segments: Vec<[na::Point3<f32>; 2]> =
+            [[false, false], [false, true], [true, true], [true, false]]
+                .iter()
+                .circular_tuple_windows()
+                .take(4)
+                .flat_map(|(pbits, nbits)| {
+                    let [px, py] = *pbits;
+                    let [nx, ny] = *nbits;
+
+                    [
+                        [[px, py, false], [nx, ny, false]],
+                        [[px, py, true], [nx, ny, true]],
+                        [[px, py, false], [px, py, true]],
+                    ]
+                })
+                .map(|[pbits, nbits]| {
+                    let bit2one = |bit: bool| {
+                        if bit {
+                            1f32
+                        } else {
+                            -1f32
+                        }
+                    };
+                    let to_point = |[xbit, ybit, zbit]: [bool; 3]| {
+                        let point = na::Point3::new(
+                            bit2one(xbit) * sx,
+                            bit2one(ybit) * sy,
+                            bit2one(zbit) * sz,
+                        );
+                        transform * point
+                    };
+                    let ppoint = to_point(pbits);
+                    let npoint = to_point(nbits);
+                    [ppoint, npoint]
+                })
+                .collect();
+
+        let bbox_segments = {
+            let color = na::Point3::new(1.0, 1.0, 0.0);
+            ColoredSegmentSet { segments, color }
+        };
+
+        Object3D {
+            bbox_segments,
+            transform,
+            size_xyz: [sx, sy, sz],
+            classification: iobj.classification.clone(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, FromPrimitive)]
+#[repr(usize)]
+enum PointColorMode {
+    Uniform = 0,
+    Indensity,
+    Distance,
+    ObjectClass,
+}
+
+impl Default for PointColorMode {
+    fn default() -> Self {
+        Self::from_usize(0).unwrap()
+    }
+}
+
+impl PointColorMode {
+    pub fn next(&self) -> Self {
+        match Self::from_usize(*self as usize + 1) {
+            Some(next) => next,
+            None => Self::default(),
+        }
     }
 }
