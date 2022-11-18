ae5fcffa4dfe9a236a77f3477779a753e47f4777
aeon <jerry73204@gmail.com>
2022-11-18 19:26:15 +0800

    Use "ros2 run" instead of running binary directory in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,16 +4,15 @@ set -e
 script_dir=$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")")
 cd "$script_dir"
 
-source /opt/ros/galactic/setup.bash
-source repos/install/setup.bash
-
-# cargo build --release --all-targets
-
+if [[ ! -v ROS_DISTRO ]]; then
+    echo 'Did you source install/setup.{sh,bash,zsh,..} ?' >&2
+    exit 1
+fi
 
 parallel -j0 --lb <<EOF
 ros2 launch velodyne_driver velodyne_driver_node-VLP32C-launch.py
 ros2 launch velodyne_pointcloud velodyne_convert_node-VLP32C-launch.py
-./target/release/kneron_bbox_server_node
-./target/release/v4l2_node --config crates/v4l2_node/config/example.json5
-env DISPLAY=:0 ./target/release/camera_viz --config crates/camera_viz/config/example.json5
+ros2 run kneron_bbox_server_node kneron_bbox_server_node
+ros2 run otobrite_v4l2_node otobrite_v4l2_node -- --config $script_dir/../src/newslab_fuse_demo/otobrite_v4l2_node/config/example.json5
+ros2 run newslab_fuse_demo newslab_fuse_demo -- --config $script_dir/../src/newslab_fuse_demo/newslab_fuse_demo/config/example.json5
 EOF
